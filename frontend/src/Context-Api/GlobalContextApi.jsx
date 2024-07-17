57b046ec09f24a255fc629e80231b1243bbe64f1
RockStar <kingmadhavan2022@gmail.com>
2024-07-17 23:23:43 +0530

    first commit

---
+++ frontend/src/Context-Api/GlobalContextApi.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const GlobalText = createContext(null);
+export default function GlobalState({ children }) {
+  return (
+    <GlobalText.Provider value={{ }}>
+      {children}
+    </GlobalText.Provider>
+  );
+}
